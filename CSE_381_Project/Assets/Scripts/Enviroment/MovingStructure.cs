c88821470c6d3b2f87a7f68bb68135469d54c3df
miraj-alam6 <miraj.alam@stonybrook.edu>
2016-11-06 21:46:56 -0500

    Cleaned up code for MovingStructure
    Cleaned the code during skype session, random garbage is gone. And one more hotkey to test 3 different paths.

--- CSE_381_Project/Assets/Scripts/Enviroment/MovingStructure.cs
+++ CSE_381_Project/Assets/Scripts/Enviroment/MovingStructure.cs
@@ -14,44 +14,21 @@ public class MovingStructure : MonoBehaviour {
     //THe w coordinate of each location represents which coordinates are actually used.
     //When a coordinate is "not used", its value is not gotten from this array but is instead
     //extracted from the position of the structure's transform.
+    //w is a number that ranges from 0 to 7. It encodes which coordinates are used much like how
+    //read/write/exec permissions are encoded in UNIX.
+    //0: none, 1: X, 2: Y, 3: X, Y, 4: Z, 5: X, Z, 6: Y,Z, 7: X,Y,Z
     public Vector4[] pathLocations;
     public float[] pathSpeeds;
     public bool[] pathGotoLatter;
     public float[] pathGoalWaitingTimes;
     float[] pathGoalTimeLeft;
-    //GameObject actualPlatform, commented out, because it is never used
-    //public GameObject actualPlatform;
 
 
-    //float[] locations
-    //The following array will hold points for every location that the platform can
-    //possibly move to. This is a striped array. It is striped on two different levels.
-    //There are quintuplets that each represent two points. And two consecutive quintuplets
-    //represent the two different points in one path. Once the platform reaches one of the points
-    //in the path, it will move towards the other point in the pair, and this will keep repeating.
-    public float[] locations;
-    //How the 5-tuple works: xyzag. xyz: coordinates, 
-    //a is a number that tells you which coordinates are active as in, which
-    //coordinates to use value for from xyz. Values that we don't use xyz values for will get their
-    //coordinate value from whatever it is in the transform of the platform.
-    //a is a number that ranges from 0 to 7. It encodes which coordinates are used much like how
-    //read/write/exec permissions are encoded in UNIX.
-    //0: none, 1: X, 2: Y, 3: X, Y, 4: Z, 5: X, Z, 6: Y,Z, 7: X,Y,Z
-    //g tells you 
-
-
-    //int pathIndex :this says which path the platform will be following, as in which pair
-    //of 5-tuples in the locations array is the active pair.
+    //int pathIndex :this says which path the platform will be following, as in which pair of points
     public int pathIndex; 
-    public bool forNow = true;
     public Transform trans;
-    int firstPointStartIndex;
-    int secondPointStartIndex;
-    //The following boolean will be used to determine when to switch the path. If this is on,
-    //the move function will decide to recalcualte where point1 and point2 is to correspond to
-    //the new path. 
-    private bool switchMovement = false;
-    
+
+    public bool activated = true;
     
     // Initializes the game object to get its Transform component
     //Also set up which two points will be used for the current path
@@ -64,15 +41,7 @@ public class MovingStructure : MonoBehaviour {
         {
             pathGoalTimeLeft[i] = pathGoalWaitingTimes[i];
         }
-        /*
-        dest1.x = transform.position.x;
-        dest1.y = transform.position.y;
-        dest1.z = transform.position.z;
-
-        dest2.x = transform.position.x + 10;
-        dest2.y = transform.position.y;
-        dest2.z = transform.position.z;
-        */
+        
     }
 
     // Update is called once per frame
@@ -84,8 +53,14 @@ public class MovingStructure : MonoBehaviour {
         {
             setupActivePoints(1);
         }
-        move();
-	}
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            setupActivePoints(2);
+        }
+        if (activated) { 
+            move();
+        }
+    }
 
 
     //Call this function once your platform/block switches its state so that you 
@@ -140,7 +115,7 @@ public class MovingStructure : MonoBehaviour {
                 point.y = pathLocations[index].y;
                 point.z = trans.position.z;
                 break;
-            case 4:
+            case 4: // Z will be set
                 point.x = trans.position.x;
                 point.y = trans.position.y;
                 point.z = pathLocations[index].z;
@@ -170,58 +145,53 @@ public class MovingStructure : MonoBehaviour {
     //towards its destination. It will be called every frame. The movement is smooth
     //Vector3.MoveTowards is used
     public void move() {
-        if (switchMovement)
-        {
-            switchMovement = false;
-        }
+        
 
-        //If you don't have to switch your path, then move along your current path
-        else {
-            //Each path has two points, figure out which point is your goal with the 5th element
-            //of each quintuplet.
-            if (!pathGotoLatter[pathIndex]) {
-                //now first check if you have reached your goal. 
-                if (Vector3.Distance(trans.position, point1) <= 0.1)
-                {
-                    if (pathGoalTimeLeft[pathIndex] <= 0) {
-                        //Reset the waiting time, and change the goal to go to
-                        pathGoalTimeLeft[pathIndex] = pathGoalWaitingTimes[pathIndex];
-                        pathGotoLatter[pathIndex] = true;
-
-                    }
-                    pathGoalTimeLeft[pathIndex] -= Time.deltaTime;
-                }
-
-                //Now if you didn't reach your goal, move towards it
-                else {
-                    trans.position = Vector3.MoveTowards(transform.position, point1, Time.deltaTime * speed);
-                }
-
-            }
-
-            else if (pathGotoLatter[pathIndex])
+        
+        //Each path has two points, figure out which point is your goal.
+        if (!pathGotoLatter[pathIndex]) {
+            //now first check if you have reached your goal. 
+            if (Vector3.Distance(trans.position, point1) <= 0.1)
             {
-                //now first check if you have reached your goal. 
-                if (Vector3.Distance(trans.position, point2) <= 0.1)
-                {
-                    if (pathGoalTimeLeft[pathIndex] <= 0)
-                    {
-                        //Reset the waiting time, and change the goal to go to
-                        pathGoalTimeLeft[pathIndex] = pathGoalWaitingTimes[pathIndex];
-                        pathGotoLatter[pathIndex] = false;
+                if (pathGoalTimeLeft[pathIndex] <= 0) {
+                    //Reset the waiting time, and change the goal to go to
+                    pathGoalTimeLeft[pathIndex] = pathGoalWaitingTimes[pathIndex];
+                    pathGotoLatter[pathIndex] = true;
 
-                    }
-                    pathGoalTimeLeft[pathIndex] -= Time.deltaTime;
                 }
+                pathGoalTimeLeft[pathIndex] -= Time.deltaTime;
+            }
 
-                //Now if you didn't reach your goal, move towards it
-                else {
-                    trans.position = Vector3.MoveTowards(transform.position, point2, Time.deltaTime * speed);
-                }
-
+            //Now if you didn't reach your goal, move towards it
+            else {
+                trans.position = Vector3.MoveTowards(transform.position, point1, Time.deltaTime * speed);
             }
 
         }
+
+        else if (pathGotoLatter[pathIndex])
+        {
+            //now first check if you have reached your goal. 
+            if (Vector3.Distance(trans.position, point2) <= 0.1)
+            {
+                if (pathGoalTimeLeft[pathIndex] <= 0)
+                {
+                    //Reset the waiting time, and change the goal to go to
+                    pathGoalTimeLeft[pathIndex] = pathGoalWaitingTimes[pathIndex];
+                    pathGotoLatter[pathIndex] = false;
+
+                }
+                pathGoalTimeLeft[pathIndex] -= Time.deltaTime;
+            }
+
+            //Now if you didn't reach your goal, move towards it
+            else {
+                trans.position = Vector3.MoveTowards(transform.position, point2, Time.deltaTime * speed);
+            }
+
+        }
+
+        
         
     }
 
