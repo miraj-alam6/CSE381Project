02c61c8e9d7ef3466a49db8431b59969907007c7
Alden <aldenpeffer@gmail.com>
2016-11-14 13:50:46 -0500

    Added GUI button on click will load new screen (script, not fully implemented to each button case).

---
+++ CSE_381_Project/Assets/ButtonEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonEventHandler : MonoBehaviour {
+
+	public GameObject mainScreen;
+	public GameObject levelSelectScreen;
+	public GameObject howToPlayScreen;
+	private GameObject currentScreen;
+
+	// Initialized to main screen display, others disabled
+	void Start () {
+		mainScreen.SetActive (true);
+		levelSelectScreen.SetActive (false);
+		howToPlayScreen.SetActive (false);
+		currentScreen = mainScreen;
+	}
+
+	public void LoadLevelSelectScreen(){
+		currentScreen.SetActive (false);
+		levelSelectScreen.SetActive (true);
+		currentScreen = levelSelectScreen;
+	}
+
+	public void LoadMainScreen(){
+		currentScreen.SetActive (false);
+		mainScreen.SetActive (true);
+		currentScreen = mainScreen;
+	}
+
+	public void LoadHowToPlayScreen(){
+		currentScreen.SetActive (false);
+		howToPlayScreen.SetActive (true);
+		currentScreen = howToPlayScreen;
+	}
+
+}
