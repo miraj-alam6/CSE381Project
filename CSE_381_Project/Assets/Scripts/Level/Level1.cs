f1f989b8c5140bed8ee94d90accf2e6d28ee15c8
miraj-alam6 <miraj.alam@stonybrook.edu>
2016-12-12 16:09:53 -0500

    won't be seen

--- CSE_381_Project/Assets/Scripts/Level/Level1.cs
+++ CSE_381_Project/Assets/Scripts/Level/Level1.cs
@@ -16,30 +16,14 @@ public class Level1 : Level
 
         if (message.Equals("pick_up_artifact"))
         {
-            if (!messagesActivated[0])
-            {
-                messagesToActivate[0].gameObject.SetActive(true);
-                messagesActivated[0] = true;
-            }
-            if (!messagesActivated[1])
-            {
-                messagesToActivate[1].gameObject.SetActive(true);
-                messagesActivated[1] = true;
-            }
         }
 
         if (message.Equals("second_artifact"))
         {
-            if (!messagesActivated[2])
-            {
-                messagesToActivate[2].gameObject.SetActive(true);
-                messagesActivated[2] = true;
-            }
         }
 
         if (message.Equals("level_done"))
         {
-            
             GameManager.instance.finishLevel();
             
         }
