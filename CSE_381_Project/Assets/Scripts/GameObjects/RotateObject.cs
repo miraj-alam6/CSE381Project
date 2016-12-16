3399a8edd3bdf6c216987bfc890f3798ffa40e29
miraj-alam6 <miraj.alam@stonybrook.edu>
2016-12-15 19:39:06 -0500

    skjd

---
+++ CSE_381_Project/Assets/Scripts/GameObjects/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+    public float speed;
+    //public bool active;
+	// Use this for initialization
+	void Start () {
+       	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!GameManager.instance.gamePaused) {
+            transform.Rotate(Vector3.right, speed * Time.deltaTime); 
+        }
+	}
+}
