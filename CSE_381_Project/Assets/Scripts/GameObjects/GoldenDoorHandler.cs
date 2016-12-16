9a73a2cddf7f219747c1ec13f777d6c016d2b4a6
Alden <aldenpeffer@gmail.com>
2016-12-15 19:18:57 -0500

    Level 3 completed (scene Level4), messages included

---
+++ CSE_381_Project/Assets/Scripts/GameObjects/GoldenDoorHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoldenDoorHandler : MonoBehaviour {
+
+	public GameObject goldenDoorLeft;
+	public GameObject goldenDoorRight;
+	public GoldenDoorScript gdLeftScript;
+	public GoldenDoorScript gdRightScript;
+	public Vector3 doorMovement;
+	public float speed;
+
+	Vector3 leftDoorGoal;
+	Vector3 rightDoorGoal;
+
+	void Start () {
+		leftDoorGoal = goldenDoorLeft.transform.position + doorMovement*-1;
+		rightDoorGoal = goldenDoorRight.transform.position + doorMovement;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (gdLeftScript.isActivated && gdRightScript.isActivated) {
+			//open doors
+			print("Open sesame");
+			goldenDoorLeft.transform.position = Vector3.MoveTowards (goldenDoorLeft.transform.position, leftDoorGoal,
+				Time.deltaTime * speed);
+
+			goldenDoorRight.transform.position = Vector3.MoveTowards (goldenDoorRight.transform.position, rightDoorGoal,
+				Time.deltaTime * speed);
+
+		}
+	}
+}
